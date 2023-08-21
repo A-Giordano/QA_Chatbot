474244600ce05f128a76de13f33d56a4021004d9
A-Giordano <andrea.giordano991@gmail.com>
2023-08-21 13:22:17 +0200

    updated chainlit usage

--- app.py
+++ app.py
@@ -3,10 +3,57 @@ from chatbot_agent import get_agent
 import uuid
 
 
-@cl.langchain_factory(use_async=False)
-def factory():
+@cl.on_chat_start
+def init():
     # prompt = PromptTemplate(template=template, input_variables=["question"])
     # llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
     index_name = "qa-chatbot"
+    chain = get_agent(index_name)
+    cl.user_session.set("chain", chain)
 
-    return get_agent(index_name)
+
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain")  # type: RetrievalQAWithSourcesChain
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message, callbacks=[cb])
+
+    answer = res["answer"]
+    sources = res["sources"].strip()
+    source_elements = []
+
+    # Get the metadata and texts from the user session
+    metadatas = cl.user_session.get("metadatas")
+    all_sources = [m["source"] for m in metadatas]
+    texts = cl.user_session.get("texts")
+
+    if sources:
+        found_sources = []
+
+        # Add the sources to the message
+        for source in sources.split(","):
+            source_name = source.strip().replace(".", "")
+            # Get the index of the source
+            try:
+                index = all_sources.index(source_name)
+            except ValueError:
+                continue
+            text = texts[index]
+            found_sources.append(source_name)
+            # Create the text element referenced in the message
+            source_elements.append(cl.Text(content=text, name=source_name))
+
+        if found_sources:
+            answer += f"\nSources: {', '.join(found_sources)}"
+        else:
+            answer += "\nNo sources found"
+
+    if cb.has_streamed_final_answer:
+        cb.final_stream.elements = source_elements
+        await cb.final_stream.update()
+    else:
+        await cl.Message(content=answer, elements=source_elements).send()
+
