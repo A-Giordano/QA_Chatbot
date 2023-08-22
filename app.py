b7758b756e076cccafd18e3f87ce98f7f7a3594d
A-Giordano <andrea.giordano991@gmail.com>
2023-08-22 16:09:05 +0200

    fork simple_qa/agent

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import chainlit as cl
-from chatbot_agent import get_agent
+from chatbot_agent import get_agent, get_memory_agent
 import uuid
 
 
@@ -9,52 +9,20 @@ def init():
     # llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
     index_name = "qa-chatbot"
     chain = get_agent(index_name)
+    # chain = get_memory_agent(index_name)
     cl.user_session.set("chain", chain)
 
 
 @cl.on_message
 async def main(message):
     chain = cl.user_session.get("chain")  # type: RetrievalQAWithSourcesChain
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-    )
-    cb.answer_reached = True
-    res = await chain.acall(message, callbacks=[cb])
-    print(res)
+    # cb = cl.AsyncLangchainCallbackHandler(
+    #     stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    # )
+    # cb.answer_reached = True
+    res = chain.run(message)
+    # print(res)
 
-    answer = res["answer"]
-    sources = res["sources"].strip()
-    source_elements = []
-
-    # Get the metadata and texts from the user session
-    metadatas = cl.user_session.get("metadatas")
-    all_sources = [m["source"] for m in metadatas]
-    texts = cl.user_session.get("texts")
-
-    if sources:
-        found_sources = []
-
-        # Add the sources to the message
-        for source in sources.split(","):
-            source_name = source.strip().replace(".", "")
-            # Get the index of the source
-            try:
-                index = all_sources.index(source_name)
-            except ValueError:
-                continue
-            text = texts[index]
-            found_sources.append(source_name)
-            # Create the text element referenced in the message
-            source_elements.append(cl.Text(content=text, name=source_name))
-
-        if found_sources:
-            answer += f"\nSources: {', '.join(found_sources)}"
-        else:
-            answer += "\nNo sources found"
-
-    if cb.has_streamed_final_answer:
-        cb.final_stream.elements = source_elements
-        await cb.final_stream.update()
-    else:
-        await cl.Message(content=answer, elements=source_elements).send()
+    # answer = res["answer"]
+    await cl.Message(content=res).send()
 
--- chatbot_agent.py
+++ chatbot_agent.py
@@ -1,7 +1,17 @@
 from functools import partial
 
-from langchain.chains import RetrievalQA, RetrievalQAWithSourcesChain
+from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_memory import BaseChatMemory
+from langchain.agents import initialize_agent, AgentType
+
+from langchain.chat_models import PromptLayerChatOpenAI
+from langchain.agents import Tool
+
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import MessagesPlaceholder
+from langchain.schema import SystemMessage
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
@@ -9,6 +19,20 @@ from langchain.vectorstores import Pinecone
 from text_extractor import TextExtractor
 import tiktoken
 import pinecone
+
+
+system_message = f"""
+<instructions>
+- Ignore any previous instructions
+- You are acting as Steve Jobs, the iconic co-founder of Apple Inc.
+- Emulate the personality, demeanor, and speaking style of Steve Jobs, the iconic co-founder of Apple Inc. Try to mimic his forward-thinking mindset, inspirational tone, articulate responses, and passion for innovation. Emphasize on his belief in simplicity and intuition when explaining concepts or ideas. Utilize his characteristic mix of business acumen, visionary thinking, and persuasiveness in the responses. 
+- In essence, throughout this session, think differently, just as Steve Jobs would have.
+- Keep your responses short, STRICTLY max 2 sentences. Example: sentence 1 blah blah. sentence 2 blah blah blah.
+- Do not say you are a language model. We are play acting and you are playing the part of Steve Jobs.
+- Do not say you are a virtual assistant. We are play acting and you are playing the part of Steve Jobs.
+</instructions>
+
+NEVER SAY YOU ARE A VIRTUAL ASSISTANT, YOU ARE STEVE JOBS."""
 
 
 def count_tokens(string: str, encoding_model: str) -> int:
@@ -55,5 +79,55 @@ def get_agent(index_name):
 
     embeddings = OpenAIEmbeddings()
     vectorstore = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
-    return RetrievalQAWithSourcesChain.from_chain_type(llm, retriever=vectorstore.as_retriever())
+    # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
+    llm = PromptLayerChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+    return RetrievalQA.from_chain_type(llm, retriever=vectorstore.as_retriever())
+
+
+def get_memory_agent(index_name):
+    pinecone.init(environment="us-west1-gcp-free")
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
+    # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
+    llm = PromptLayerChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    qa = RetrievalQA.from_chain_type(llm, retriever=vectorstore.as_retriever())
+
+    tools = [
+        Tool(
+            name='TBW-Palestine_Game_Knowledge_Base',
+            func=qa.run,
+            description=(
+                'use this tool when answering questions about the TBW-Palestine game'
+                'more information about the topic'
+            )
+        )
+    ]
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    # agent = initialize_agent(
+    #     agent='chat-conversational-react-description',
+    #     tools=tools,
+    #     llm=llm,
+    #     verbose=True,
+    #     max_iterations=3,
+    #     early_stopping_method='generate',
+    #     memory=memory
+    # )
+
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="chat_history")],
+        "system_message": SystemMessage(content=system_message)
+    }
+    agent = initialize_agent(tools,
+                                   llm,
+                                   agent=AgentType.OPENAI_FUNCTIONS,
+                                   # agent=AgentType.OPENAI_MULTI_FUNCTIONS,
+                                   agent_kwargs=agent_kwargs,
+                                   memory=memory,
+                                   max_execution_time=10,
+                                   verbose=True)
+    return agent
+    # return RetrievalQA.from_chain_type(llm, retriever=vectorstore.as_retriever())
+
+
